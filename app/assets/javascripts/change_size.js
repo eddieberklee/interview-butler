f133e4cfccc0a96b9118171127563626747e31cb
Eddie Lee <eddieberklee@gmail.com>
2014-04-20 20:10:45 -0700

    working buildiung out the timeline now

--- app/assets/javascripts/change_size.js
+++ app/assets/javascripts/change_size.js
@@ -1,8 +1,10 @@
 
 $(document).ready(function() {
 	$("#change-size.pagination li").click(function() {
+		event.preventDefault();
 		var size = $(this).attr('class');
 		$(".company.window").each(function() {
+			console.log($(this));
 			$(this).attr('class','company window');
 			$(this).addClass('col-sm-' + size);
 		});
